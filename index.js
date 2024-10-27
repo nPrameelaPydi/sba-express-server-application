36681389fb75e614044fc5ace0c0b7c5eea81cba
Prameela Pydi <nprameela.pydi@gmail.com>
2024-10-27 08:42:06 -0700

    custom middleware

--- index.js
+++ index.js
@@ -2,6 +2,43 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 
+
+// Middleware
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ extended: true }))
+
+//Request Timing Middleware
+//Logs how long each request takes to process.
+const requestTimer = (req, res, next) => {
+    const start = Date.now();
+    res.on("finish", () => {
+        const duration = Date.now() - start;
+        console.log(`${req.method} ${req.url} took ${duration}ms`);
+    });
+    next();
+};
+app.use(requestTimer);
+
+//Middleware to log request details
+const requestLogger = (req, res, next) => {
+    const localTime = new Date().toLocaleString(); // Get server local time and date
+    console.log(`[${localTime}] ${req.method} request to ${req.url}`);
+    next();
+};
+app.use(requestLogger);
+
+
+
+
+
+
+
+
+
+
+
+
+
 //base route
 app.get("/", (req, res) => {
     res.send("Welcome to the SBA App!");
